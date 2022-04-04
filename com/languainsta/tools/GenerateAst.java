6aba18a9b7bff132de897c3e8e21c8cb52181856
yatinbawa1 <yatinabc3@gmail.com>
2022-04-04 23:29:00 +0530

    added Visitor pattern

--- com/languainsta/tools/GenerateAst.java
+++ com/languainsta/tools/GenerateAst.java
@@ -24,12 +24,12 @@ public class GenerateAst {
             List<String> types) throws IOException {
         String path =  outputDir + "/" + baseName + ".java";
         PrintWriter writer = new PrintWriter(path, "UTF-8");
-
         writer.println("package com.languainsta.lox");
         writer.println();
         writer.println("import java.util.List;");
         writer.println();
         writer.println("abstract class " + baseName + " {");
+        defineVisitor(writer,baseName,types);
         // The AST Classes
         for (String type: types) {
             String className = type.split(":")[0].trim();
@@ -37,18 +37,30 @@ public class GenerateAst {
             defineType(writer, baseName,className, fields);
         }
 
+        // The base accept() method.
+        writer.println();
+        writer.println("  abstract <R> R accept(Visitor<R> visitor);");
+
         writer.println("}");
         writer.close();
+    }
+
+    public static void defineVisitor(PrintWriter writer, String baseName
+                        ,List<String> types) {
+        for (String type: types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("    R visit" + typeName + baseName + "(" +
+                    typeName + " " + baseName.toLowerCase() + ");");
+        }
+        writer.println("}");
     }
 
     public static void defineType( PrintWriter writer, String baseName,
             String className, String fieldList) {
 
-        writer.println();
         writer.println("    static class " + className + " extends " +
                 baseName + "{");
         //constructor
-        writer.println();
         writer.println("        " + className + "(" + fieldList + ")" + "{");
         String[] fields = fieldList.split(",");
         for (String field: fields){
@@ -57,8 +69,15 @@ public class GenerateAst {
         }
         writer.println("        }");
 
-        // fields
+        // Visitor pattern.
         writer.println();
+        writer.println("    @Override");
+        writer.println("    <R> R accept(Visitor<R> visitor) {");
+        writer.println("      return visitor.visit" +
+                className + baseName + "(this);");
+        writer.println("    }");
+
+        // fields
         for (String field: fields) {
             writer.println("        final " + field + ";");
         }
