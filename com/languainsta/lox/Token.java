50bd45ab63ef3feff24e01b0e3cbb48314ca3b4b
yatinbawa1 <yatinabc3@gmail.com>
2022-04-05 01:03:03 +0530

    Saving State

--- com/languainsta/lox/Token.java
+++ com/languainsta/lox/Token.java
@@ -2,8 +2,12 @@ package com.languainsta.lox;
 
 // datatype token created using records
 record Token(TokenType type, String lexeme, Object literal, int line) {
-
     public String toString() {
         return type + " " + lexeme + " " + literal;
+    }
+
+    @Override
+    public String lexeme() {
+        return lexeme;
     }
 }
