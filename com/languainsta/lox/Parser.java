780f52937a059ba88e8bf238cdbb2d94403a3bbf
yatinbawa1 <yatinabc3@gmail.com>
2022-04-10 13:46:49 +0530

    Added Comment in Parser file, being able to understand it better it is explained with a example as well

--- com/languainsta/lox/Parser.java
+++ com/languainsta/lox/Parser.java
@@ -6,38 +6,22 @@ import static com.languainsta.lox.TokenType.*;
 
 class Parser {
     private static class ParseError extends RuntimeException {}
-
     private final List<Token> tokens;
+    // current refers to the token
+    // next in line to be used
     private int current = 0;
 
+    // Initialize List of tokens
     Parser(List<Token> tokens) {
         this.tokens = tokens;
     }
 
-    private Expr expression() {
-        return equality();
+    private Token peek() {
+        return tokens.get(current);
     }
 
-    private Expr equality() {
-
-        Expr expr = comparison();
-        while (match(BANG_EQUAL,EQUAL_EQUAL)) {
-            Token operator = previous();
-            Expr right = comparison();
-            expr = new Expr.Binary(expr,operator,expr);
-        }
-
-        return expr;
-    }
-
-    private boolean match(TokenType... types) {
-        for (TokenType type: types) {
-            if (check(type)) {
-                advance();
-                return true;
-            }
-        }
-        return false;
+    private boolean isAtEnd() {
+        return peek().type() == EOF;
     }
 
     private boolean check(TokenType type) {
@@ -45,22 +29,67 @@ class Parser {
         return peek().type() == type;
     }
 
+    private Token previous() {
+        return  tokens.get(current - 1);
+    }
+
     private Token advance() {
         if (!isAtEnd()) current++;
         return previous();
     }
-
-    private boolean isAtEnd() {
-        return peek().type() == EOF;
+    // match check if current token
+    // is something that
+    // matches any of the given
+    // TokenTypes
+    private boolean match(TokenType... types) {
+        for (TokenType type: types) {
+            if (check(type)) {
+//                if it does match
+//                we advance our current
+//                and then return true
+                advance();
+                return true;
+            }
+        }
+        return false;
     }
 
-    private Token peek() {
-        return tokens.get(current);
+
+//   "25*36-45 <= 850 == true" -> Binary(Binary(Binary(Binary(25,STAR,36),MINUS,45),LESS_EQUAL,850),EQUAL_EQUAL,TRUE)
+//                                              EQUAL_EQUAL
+//                                                  ^
+//                                      LESS_EQUAL    TRUE
+//                                        ^   <-
+//                                      MINUS 850
+//                                        ^   <-
+//                                      STAR  45
+//                                       ^    <-
+//                                      25    36
+//   -----------------------------------------------------------------------------------------------------------------
+    private Expr expression() {
+        return equality();
     }
 
-    private Token previous() {
-        return  tokens.get( current -1);
+//    example 25*36-45 <= 850 == true
+//    expr = comparison() -> Binary(Binary(Binary(25,STAR,36),MINUS,45),LESS_EQUAL,850)
+//    matches EQUAL_EQUAL
+//    right = comparison() -> term -> factor -> unary() -> primary() -> TRUE
+//    returns -> Binary(Binary(Binary(Binary(25,STAR,36),MINUS,45),LESS_EQUAL,850),EQUAL_EQUAL,TRUE)
+    private Expr equality() {
+        Expr expr = comparison();
+        while (match(BANG_EQUAL,EQUAL_EQUAL)) {
+            Token operator = previous();
+            Expr right = comparison();
+            expr = new Expr.Binary(expr,operator,expr);
+        }
+        return expr;
     }
+
+//    example 25*36-45 <= 850
+//    expr = term() -> factor -> Binary(Binary(25,STAR,36),MINUS,45)
+//    matches LESS_EQUAL
+//    right = term() -> factor() -> unary() -> primary() -> 850
+//    returns Binary(Binary(Binary(25,STAR,36),MINUS,45),LESS_EQUAL,850)
     private Expr comparison() {
         Expr expr = term();
 
@@ -72,7 +101,11 @@ class Parser {
 
         return expr;
     }
-
+//    example 25*36-45
+//    expr = factor() -> Binary(25,STAR,36)
+//    matched minus
+//    right = factor() -> unary() - primary() -> 45
+//    returns -> Binary(Binary(25,STAR,36),MINUS,45)
     private Expr term() {
         Expr expr = factor();
 
@@ -84,7 +117,11 @@ class Parser {
 
         return expr;
     }
-
+    // example 25*36
+    // expr = unary() -> primary() -> 25
+    // matched star
+    // right = unary() -> primary() -> 36
+    // returns Binary(25,STAR,36)
     private Expr factor() {
         Expr expr = unary();
 
@@ -97,6 +134,12 @@ class Parser {
         return expr;
     }
 
+    // an example -25
+    // minus is matched
+    // by match()
+    // currentValue = 25;
+    // when right = Unary() -> return primary();
+    // primary() -> Literal 25
     private Expr unary() {
         if (match(BANG,MINUS)) {
             Token operator = previous();
